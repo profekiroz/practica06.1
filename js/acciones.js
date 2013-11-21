793b00f856ba91cfea2811c0600aba7e0e32a73e
Armando Quiroz Cortes <akiroz99@gmail.com>
2013-11-21 17:00:01 -0600

    2111g

--- js/acciones.js
+++ js/acciones.js
@@ -45,6 +45,47 @@ var db = openDatabase("Test", "1.0","Test",65535);
   });
 });
 	
+$("#listar").bind ("click", function (event)
+{
+  db.transaction (function (ejecutar) 
+  {
+    var sql = "SELECT * FROM clientes";
+
+    ejecutar.executeSql (sql, undefined,
+
+    function (ejecutar, resultado)
+    {
+      var a_html = "<ul>";
+      if (resultado.rows.length)
+      {
+        for (var i = 0; i < resultado.rows.length; i++) 
+        {
+          var fila = resultado.rows.item (i);
+          var v_nombre = fila.nombre;
+          var v_apellido = fila.apellido;
+          a_html += "<li>" + v_nombre + "&nbsp;" + v_apellido + "</li>";
+        }
+      }
+      else
+      {
+        a_html += "<li> No hay clientes </li>";
+      }
+      
+      a_html += "</ul>";
+      
+      $("#listado").unbind ().bind ("pagebeforeshow", function ()
+      {
+        var $contenido = $("#listado div:jqmData(role=content)");
+        $contenido.html (a_html);
+        var $ul = $contentenido.find ("ul");
+        $ul.listview ();
+      });
+      
+      $.mobile.changePage ($("#listado"));
+      
+    }, error);
+  });	
+});	
 	function error (transaction, err) 
 {
   alert ("Error de Base de Datos : " + err.message);
