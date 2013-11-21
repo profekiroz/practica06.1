6f6d34c943fc6220bedc51f456665c0a61d79dbd
Armando Quiroz Cortes <akiroz99@gmail.com>
2013-11-21 16:29:55 -0600

    2111d

--- js/acciones.js
+++ js/acciones.js
@@ -4,11 +4,11 @@ $(document).ready(function(e){
 	document.addEventListener("deviceready",function(){
 		
 		
-var db=OpendataBase("Test", "1.0","Test",65535);
-alert("Listo");
+var db = openDatabase("Test", "1.0","Test",65535);
+
 
 	$('#Crear').bind("Click",function(event){
-		alert("ok");
+		
 		db.transaction(function(ejecutar){
 			var SQL="CREATE TABLE Clientes(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(64) NOT NULL, apellido VARCHAR(100))"
 	
@@ -19,7 +19,7 @@ alert("Listo");
 			});//crear
 	
 	$('#Eliminar').bind("Click",function(event){
-		alert("Va");
+	
 		if (!confirm("Borrar tabla?",""))
 		return;
 		db.transaction(function(ejecutar){
@@ -28,7 +28,22 @@ alert("Listo");
 				alert("Tabla borrada");
 			},error);
 			});//ejecutar		
-			});
+			});//clic eliminar
+			
+			$("#insertar").bind ("click", function (event)
+{
+  var v_nombre = $("#nombre").val ();
+  var v_apellido = $("#apellido").val ();
+  
+  db.transaction (function (ejecutar) 
+  {
+    var sql = "INSERT INTO clientes (nombre, apellido) VALUES (?, ?)";
+    ejecutar.executeSql (sql, [v_nombre, v_apellido], function ()
+    { 
+      alert ("Cliente Agregado");
+    }, error);
+  });
+});
 	
 	function error (transaction, err) 
 {
